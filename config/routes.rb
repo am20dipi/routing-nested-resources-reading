c943e576a2c4e365dbaedbfa64473e386559cc20
Adriana <am20dipi@alum.siena.edu>
2021-04-26 13:13:02 -0400

    commit tree db490dbe1a2f94e0be6a9c5f6436a172689539b5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    # nested resource for posts; nested resources give us a way to document parent/child relationship in our routes/urls
+    resources :posts, only: [:show, :index]
+    # we are passing the resource :authors a block of nested routes
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
